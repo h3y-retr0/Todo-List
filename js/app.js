dd88f0aa7c39742a8f78cded40209ca16e8a3fed
Jorge Gómez <jgespiga@gmail.com>
2022-08-07 19:13:32 +0200

    funcion addtodo implementada

--- js/app.js
+++ js/app.js
@@ -1,30 +1,55 @@
 
-'use strict'
 
-const sendTodoDiv = document.getElementById('send')
-const sendBtn = document.getElementById('sendBtn')
-const input = document.getElementById('todoInput')
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict'
 
+    const sendTodoDiv = document.getElementById('send')
+    const sendBtn = document.getElementById('sendBtn')
+    const input = document.getElementById('todoInput')
+    const deleteBtn = document.getElementsByClassName('delete')
 
-input.addEventListener('keyup', () => {
-    if(parseInt(input.value.length) >= 2) {
-        sendBtn.style.fill = '#3d72fe'
-    } else {
+    input.addEventListener('keyup', () => {
+        if(parseInt(input.value.length) >= 2) {
+            sendBtn.style.fill = '#3d72fe'
+            sendBtn.style.cursor = 'pointer'
+        } else {
+            sendBtn.style.fill = 'gray'
+            sendBtn.style.cursor = 'text'
+        }
+    })
+
+    sendBtn.addEventListener('click', () => {
+        // Change Border
+        sendTodoDiv.style.borderBottomColor = 'gray'
+        sendTodoDiv.style.borderBottomWidth = '1px'
+        /****************************************/
+        addTodo(input.value)
+        input.value = ''
         sendBtn.style.fill = 'gray'
-    }
-})
+        sendBtn.style.cursor = 'text'
+       
+    })
 
-sendBtn.addEventListener('click', () => {
-    // Change Border
-    sendTodoDiv.style.borderBottomColor = 'gray'
-    sendTodoDiv.style.borderBottomWidth = '1px'
-    /****************************************/
+    // Funcionalidad al pulsar enter
     
-})
+    input.addEventListener('keyup', (e) => {
+        if(e.key === 'Enter') {
+            addTodo(input.value)
+        } else {
+            return
+        }
+    })
 
-input.addEventListener('click', () => {
+    input.addEventListener('click', () => {
 
-    //Change border to blue
-    sendTodoDiv.style.borderBottomColor = '#3d72fe'
-    sendTodoDiv.style.borderBottomWidth = '2px'
+        //Change border to blue
+        sendTodoDiv.style.borderBottomColor = '#3d72fe'
+        sendTodoDiv.style.borderBottomWidth = '2px'
+    })
+
+    deleteBtn[0].addEventListener('click', () => {
+        deleteTodo()
+    })
+   
+
 })
--- js/todo.js
+++ js/todo.js
@@ -1,12 +1,41 @@
 
-document.addEventListener('DOMContentLoaded', () => {
+const input = document.getElementById('todoInput')
+const todoContainer = document.getElementById('wrapper')
 
-    
-    const addTodo = () => {
-        if(input.value.lenght === 0) {
+    const addTodo = (value) => {
+        if(parseInt(input.value.length) < 2 || input.value === '') {
+            console.error("Task name is required")
             return
         } else {
             // Add todo
+            
+            let container = document.createElement('div')
+            container.setAttribute('id', 'Todo')
+            container.classList.add('Todo')
+            todoContainer.appendChild(container)
+           
+
+            container.innerHTML = `
+                <button class="checkShadow">
+                    <span id="checkContainer">
+                        <input id="check" type="checkbox"/>
+                    </span>
+                </button>
+            
+                <input id="edit" type="text" placeholder="Edit your todo item here" value="${value}"/>
+                <button class="delete">
+                    <svg id="bin" class="MuiSvgIcon-root MuiSvgIcon-colorError" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg>
+                </button>
+            `
+
+            var id = 0
+            var id = id + 1
+            console.log(id)
+            
         }
     }
-})
+
+    const deleteTodo = () => {
+        let items = document.getElementsByClassName('Todo')
+        console.log()
+    }
