9a815e2ddf1c522ae1d3bd1171c08f0e0457eaf6
Jorge Gómez <jgespiga@gmail.com>
2022-08-08 13:34:35 +0200

    remove functiion

--- js/app.js
+++ js/app.js
@@ -6,50 +6,53 @@ document.addEventListener('DOMContentLoaded', () => {
     const sendTodoDiv = document.getElementById('send')
     const sendBtn = document.getElementById('sendBtn')
     const input = document.getElementById('todoInput')
-    const deleteBtn = document.getElementsByClassName('delete')
+    
 
     input.addEventListener('keyup', () => {
         if(parseInt(input.value.length) >= 2) {
             sendBtn.style.fill = '#3d72fe'
             sendBtn.style.cursor = 'pointer'
         } else {
-            sendBtn.style.fill = 'gray'
+            sendBtn.style.fill = 'rgba(0, 0, 0, 0.26)'
             sendBtn.style.cursor = 'text'
         }
     })
 
     sendBtn.addEventListener('click', () => {
-        // Change Border
-        sendTodoDiv.style.borderBottomColor = 'gray'
-        sendTodoDiv.style.borderBottomWidth = '1px'
-        /****************************************/
+   
         addTodo(input.value)
         input.value = ''
-        sendBtn.style.fill = 'gray'
+        sendBtn.style.fill = 'rgba(0, 0, 0, 0.26)'
         sendBtn.style.cursor = 'text'
        
     })
 
     // Funcionalidad al pulsar enter
-    
+
     input.addEventListener('keyup', (e) => {
         if(e.key === 'Enter') {
             addTodo(input.value)
+            input.value = ''
+            sendBtn.style.fill = 'gray'
+            sendBtn.style.cursor = 'text'
         } else {
             return
         }
     })
 
-    input.addEventListener('click', () => {
+    // Input effect
 
-        //Change border to blue
-        sendTodoDiv.style.borderBottomColor = '#3d72fe'
-        sendTodoDiv.style.borderBottomWidth = '2px'
+    input.addEventListener('focus', () => {
+       let line = document.querySelector('.inputFocused')
+       line.style.animationName = 'focused'
+       line.style.width = '100%' 
+       sendTodoDiv.style.borderBottom = '0px solid gray'       
     })
-
-    deleteBtn[0].addEventListener('click', () => {
-        deleteTodo()
+    input.addEventListener('blur', () => {
+        let line = document.querySelector('.inputFocused')
+        line.style.animationName = 'unfocused'
+        line.style.width = '0%'
+        sendTodoDiv.style.borderBottom = '1px solid gray'
     })
-   
 
 })
--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,7 @@
 
 const input = document.getElementById('todoInput')
 const todoContainer = document.getElementById('wrapper')
+let key = 0
 
     const addTodo = (value) => {
         if(parseInt(input.value.length) < 2 || input.value === '') {
@@ -10,10 +11,10 @@ const todoContainer = document.getElementById('wrapper')
             // Add todo
             
             let container = document.createElement('div')
-            container.setAttribute('id', 'Todo')
+            container.setAttribute('id', key++)
             container.classList.add('Todo')
             todoContainer.appendChild(container)
-           
+            
 
             container.innerHTML = `
                 <button class="checkShadow">
@@ -23,19 +24,21 @@ const todoContainer = document.getElementById('wrapper')
                 </button>
             
                 <input id="edit" type="text" placeholder="Edit your todo item here" value="${value}"/>
-                <button class="delete">
-                    <svg id="bin" class="MuiSvgIcon-root MuiSvgIcon-colorError" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg>
-                </button>
             `
-
-            var id = 0
-            var id = id + 1
-            console.log(id)
+            const remove = document.createElement('button')
+            remove.classList.add('delete')
+            remove.innerHTML = '<svg id="bin" class="MuiSvgIcon-root MuiSvgIcon-colorError" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg>'
+            container.appendChild(remove)
             
-        }
+            remove.onclick = () => {
+                removeTodo(container.getAttribute('id'))
+            }
+       }
     }
 
-    const deleteTodo = () => {
-        let items = document.getElementsByClassName('Todo')
-        console.log()
+    const removeTodo = (key) => {
+        let todo = document.getElementById(key)
+        todo.remove()
+        
     }
+
